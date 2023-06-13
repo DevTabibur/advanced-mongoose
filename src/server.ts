88b566c79cd2844d5f08be373d01a5c0e46165bd
unknown <tobiburrohman2@gmail.com>
2023-06-13 09:03:27 +0600

    6-3 is done

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-const express = require('express')
-const colors = require("colors");
-const app = express()
+import mongoose from 'mongoose';
+import app from './app';
 // getting-started.js
-const mongoose = require('mongoose');
-const port = 5000
+const port: number = 5000
+
+
 
 // database connection
 async function dbConnected() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
         console.log(`Database connection is successful`);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`)
+        })
 
     } catch (error) {
         console.log(`Failed to connect database`, error);
@@ -19,10 +22,5 @@ async function dbConnected() {
 
 dbConnected()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-})
+
