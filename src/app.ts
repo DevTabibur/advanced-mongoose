7f6d06462ab8bb4773801fb46a5f42e414b25e98
unknown <tobiburrohman2@gmail.com>
2023-06-13 10:18:15 +0600

    6-5 vdo is done

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     Step1: Interface
     Step2: Schema
     Step3: Model
-    Step4: Database Query
+    Step4: Database on Model Query
     */
 
     //following step1
@@ -84,9 +84,37 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     });
 
 
+    // following Step3
+    const User = model<IUser>('User', userSchema);
 
-    // res.send('Hello World!');
-    // next()
+    const createUserToDB = async () => {
+        // create instance for new user from interface
+        const user = new User(
+            {
+                id: '1234',
+                role: "student",
+                password: '1223',
+                name: {
+                    firstName: "MR. X",
+                    middleName: "Rahman",
+                    lastName: "Topu",
+                },
+                dob: "18-4-2000",
+                gender: "male",
+                email: "tobiburrohman2@gmail.com",
+                contactNo: "01307588223",
+                emergencyContactNo: "01739651860",
+                presentAddress: "Badhilki",
+                permanentAddress: "Toluigacha",
+            }
+        )
+
+        await user.save();
+        res.send({user})
+    }
+
+    createUserToDB()
+
 })
 
 export default app;
