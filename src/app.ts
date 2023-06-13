0244fc55dda54578f29313493b7905f0c192df6b
unknown <tobiburrohman2@gmail.com>
2023-06-13 11:01:20 +0600

    6-6 vdo is done

--- src/app.ts
+++ src/app.ts
@@ -23,69 +23,10 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     */
 
     //following step1
-    interface IUser {
-        id: string;
-        role: "student";
-        password: string;
-        name: {
-            firstName: string;
-            middleName?: string;
-            lastName: string;
-        }
-        dob?: string;
-        gender: 'male' | "female";
-        email?: string;
-        contactNo: string;
-        emergencyContactNo: string;
-        presentAddress: string;
-        permanentAddress: string;
-    }
+    
 
 
-    // following Step2
-    const userSchema = new Schema<IUser>({
-        id: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        role: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        name: {
-            firstName: {
-                type: String,
-                required: true,
-            },
-            middleName: {
-                type: String,
-            },
-            lastName: {
-                type: String,
-                required: true
-            }
-        },
-        dob: { type: String },
-        gender: {
-            type: String,
-            enum: ["male", "female"]
-        },
-        email: { type: String },
-        contactNo: { type: String, required: true },
-        emergencyContactNo: { type: String, required: true },
-        presentAddress: { type: String, required: true },
-        permanentAddress: { type: String, required: true },
-
-    });
-
-
-    // following Step3
-    const User = model<IUser>('User', userSchema);
+    
 
     const createUserToDB = async () => {
         // create instance for new user from interface
